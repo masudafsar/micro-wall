a9d83698567b2a7381f19bcbbb8de2fe07401b02
Masud Afsar <masud.afsar@gmail.com>
2023-08-12 12:37:35 +0330

    Use app bar in root layout

--- src/components/appBar/index.tsx
+++ src/components/appBar/index.tsx
@@ -1,20 +1,23 @@
-import { AppBar as MuiAppBar, Button, Toolbar, Typography } from '@mui/material';
+import { AppBar as MuiAppBar, Button, Container, Toolbar, Typography } from '@mui/material';
 import WebTwoToneIcon from '@mui/icons-material/WebTwoTone';
 import PlayArrowTwoToneIcon from '@mui/icons-material/PlayArrowTwoTone';
+
 export interface AppBarPropsType {}
 
 export function AppBar({}: AppBarPropsType) {
   return (
     <MuiAppBar sx={{ position: 'fixed', zIndex: 2000 }}>
-      <Toolbar>
-        <WebTwoToneIcon sx={{ mr: 2 }} />
-        <Typography variant="h6" component="h1" sx={{ whiteSpace: 'nowrap', flexGrow: 1 }}>
-          MicroWall <small>(Edit Mode)</small>
-        </Typography>
-        <Button color="success" variant="contained" endIcon={<PlayArrowTwoToneIcon />}>
-          Preview
-        </Button>
-      </Toolbar>
+      <Container>
+        <Toolbar sx={{ px: '0 !important' }}>
+          <WebTwoToneIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" component="h1" sx={{ whiteSpace: 'nowrap', flexGrow: 1 }}>
+            MicroWall <small>(Edit Mode)</small>
+          </Typography>
+          <Button color="success" variant="contained" endIcon={<PlayArrowTwoToneIcon />}>
+            Preview
+          </Button>
+        </Toolbar>
+      </Container>
     </MuiAppBar>
   );
 }

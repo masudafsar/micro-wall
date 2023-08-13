6fe4a8a2896d1c49e48ac4731c19d9f242796247
Masud Afsar <masud.afsar@gmail.com>
2023-08-14 03:27:57 +0330

    Implement editor block wrapper

--- src/components/editor/blocks/textBlock.tsx
+++ src/components/editor/blocks/textBlock.tsx
@@ -1,22 +1,10 @@
 import { TextBlockType } from '@formaloo/types/block.type';
-import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
-  Box,
-  Button,
-  IconButton,
-  Stack,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { TextField } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { AppActionEnum, useAppState } from '@formaloo/providers';
 import { BlockEnum } from '@formaloo/enums';
-import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
-import ExpandMoreTwoToneIcon from '@mui/icons-material/ExpandMoreTwoTone';
-import { useState } from 'react';
 import { blocksInfo } from '@formaloo/constants';
+import { EditorBlockWrapper } from '@formaloo/components/editorBlockWrapper';
 
 export interface TextBlockPropsType {
   data: TextBlockType;
@@ -25,7 +13,6 @@ export interface TextBlockPropsType {
 export function TextBlock({ data }: TextBlockPropsType) {
   const { icon: BlockIcon } = blocksInfo[BlockEnum.text];
   const [, appDispatch] = useAppState();
-  const [isExpandForm, setIsExpandForm] = useState<boolean>(false);
 
   const {
     register,
@@ -36,8 +23,6 @@ export function TextBlock({ data }: TextBlockPropsType) {
   });
 
   function handleFormSubmit(data: TextBlockType) {
-    setIsExpandForm(false);
-
     appDispatch({
       type: AppActionEnum.updateBlock,
       payload: {
@@ -47,57 +32,27 @@ export function TextBlock({ data }: TextBlockPropsType) {
     });
   }
 
-  function handleRemoveBlock() {
-    appDispatch({
-      type: AppActionEnum.removeBlock,
-      uuid: data.uuid,
-    });
-  }
-
   return (
-    <Accordion
-      expanded={isExpandForm}
-      onChange={(event, expanded) => {
-        setIsExpandForm(expanded);
-      }}
-    >
-      <AccordionSummary
-        id={`accordion-header-${data.uuid}`}
-        aria-controls={`accordion-content-${data.uuid}`}
-        expandIcon={<ExpandMoreTwoToneIcon />}
-      >
-        <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', flexWrap: 'nowrap', gap: 1 }}>
-          <BlockIcon />
+    <EditorBlockWrapper id={data.uuid} title={data.title} icon={BlockIcon} onSubmit={handleSubmit(handleFormSubmit)}>
+      <TextField
+        label="Title"
+        fullWidth
+        {...register('title', {
+          required: 'Title field is required.',
+        })}
+        error={Boolean(errors.title)}
+        helperText={errors.title && errors.title.message}
+      />
 
-          <Typography sx={{ flex: '1', color: 'text.secondary' }}>{data.title}</Typography>
-
-          <IconButton onClick={handleRemoveBlock}>
-            <DeleteTwoToneIcon />
-          </IconButton>
-        </Box>
-      </AccordionSummary>
-
-      <AccordionDetails>
-        <Box component="form" onSubmit={handleSubmit(handleFormSubmit)}>
-          <Stack spacing={1}>
-            <TextField label="Title" fullWidth {...register('title', {})} />
-
-            <TextField
-              label="Content"
-              fullWidth
-              {...register('content', { required: 'Content field is required.' })}
-              error={Boolean(errors.content)}
-              helperText={errors.content && errors.content.message}
-            />
-
-            <Box>
-              <Button type="submit" variant="contained">
-                Save
-              </Button>
-            </Box>
-          </Stack>
-        </Box>
-      </AccordionDetails>
-    </Accordion>
+      <TextField
+        label="Content"
+        fullWidth
+        {...register('content', {
+          required: 'Content field is required.',
+        })}
+        error={Boolean(errors.content)}
+        helperText={errors.content && errors.content.message}
+      />
+    </EditorBlockWrapper>
   );
 }
---
+++ src/components/editorBlockWrapper/index.tsx
@@ -0,0 +1,79 @@
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  IconButton,
+  Stack,
+  Typography,
+} from '@mui/material';
+import ExpandMoreTwoToneIcon from '@mui/icons-material/ExpandMoreTwoTone';
+import DeleteTwoToneIcon from '@mui/icons-material/DeleteTwoTone';
+import { FormEvent, PropsWithChildren, useState } from 'react';
+import { SvgIconComponent } from '@mui/icons-material';
+import { AppActionEnum, useAppState } from '@formaloo/providers';
+
+export interface EditorBlockWrapperPropsType {
+  id: string;
+  title: string;
+  icon?: SvgIconComponent;
+  onSubmit?: <T>(event: FormEvent<T>) => void;
+}
+
+export function EditorBlockWrapper({
+  id,
+  title,
+  icon: Icon,
+  onSubmit,
+  children,
+}: PropsWithChildren<EditorBlockWrapperPropsType>) {
+  const [, appDispatch] = useAppState();
+  const [isExpandForm, setIsExpandForm] = useState<boolean>(false);
+
+  function handleRemoveBlock() {
+    appDispatch({
+      type: AppActionEnum.removeBlock,
+      uuid: id,
+    });
+  }
+
+  return (
+    <Accordion
+      expanded={isExpandForm}
+      onChange={(event, expanded) => {
+        setIsExpandForm(expanded);
+      }}
+    >
+      <AccordionSummary
+        id={`accordion-header-${id}`}
+        aria-controls={`accordion-content-${id}`}
+        expandIcon={<ExpandMoreTwoToneIcon />}
+      >
+        <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', flexWrap: 'nowrap', gap: 1 }}>
+          <Icon />
+
+          <Typography sx={{ flex: '1', color: 'text.secondary' }}>{title}</Typography>
+
+          <IconButton onClick={handleRemoveBlock}>
+            <DeleteTwoToneIcon />
+          </IconButton>
+        </Box>
+      </AccordionSummary>
+
+      <AccordionDetails>
+        <Box component="form" onSubmit={onSubmit}>
+          <Stack spacing={1}>
+            {children}
+
+            <Box>
+              <Button type="submit" variant="contained">
+                Save
+              </Button>
+            </Box>
+          </Stack>
+        </Box>
+      </AccordionDetails>
+    </Accordion>
+  );
+}

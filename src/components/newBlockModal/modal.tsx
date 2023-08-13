6e3ce129971a1ab93cb81be67c0e6dd342394114
Masud Afsar <masud.afsar@gmail.com>
2023-08-14 01:32:30 +0330

    Set title as required in block type

--- src/components/newBlockModal/modal.tsx
+++ src/components/newBlockModal/modal.tsx
@@ -4,6 +4,8 @@ import { AppActionEnum, useAppState } from '@formaloo/providers';
 import { BlockType } from '@formaloo/types';
 import { BlockEnum } from '@formaloo/enums';
 import { v4 as uuid } from 'uuid';
+import { BlockInfoType } from '@formaloo/types/block.type';
+import { blocksInfo } from '@formaloo/constants';
 
 export interface NewBlockModalPropsType {
   open: boolean;
@@ -14,12 +16,15 @@ export function NewBlockModal({ open, onClose }: NewBlockModalPropsType) {
   const [, appDispatch] = useAppState();
 
   function handleItemClick(type: BlockEnum) {
+    const info: BlockInfoType = blocksInfo[type];
+
     appDispatch({
       type: AppActionEnum.addNewBlock,
       payload: {
         type: type,
         data: {
           uuid: uuid(),
+          title: info.title,
         },
       } as BlockType,
     });
